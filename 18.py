1c5a4d1eca5de851a4c08c97c68f4cb07ee450d0
vanich-nikita <vanich_nikita@mail.ru>
2020-05-09 18:05:30 +0300

    5

--- 18.py
+++ 18.py
@@ -5,7 +5,6 @@ def iter_primes():
     # an iterator of all numbers between 2 and +infinity
     numbers = itertools.count(2)
 
-
     # generate primes forever
     while True:
         # get the first number from the iterator (always a prime)
@@ -15,6 +14,7 @@ def iter_primes():
         # this code iteratively builds up a chain of
         # filters...slightly tricky, but ponder it a bit
         numbers = filter(prime.__rmod__, numbers)
+
 
 for p in iter_primes():
     if p > 1000:
