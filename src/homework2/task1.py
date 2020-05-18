bc234754fb3a90f8ff6d4e878a2ee607086ea321
vanich-nikita <vanich_nikita@mail.ru>
2020-05-18 18:36:55 +0300

    homework2 version 1.2

--- src/homework2/task1.py
+++ src/homework2/task1.py
@@ -1,27 +1,36 @@
-"""Напишите программу, которая считает общую цену. Вводится M рублей и N копеек,
- а также количество S товара Посчитайте общую цену в рублях и копейках
+"""Напишите программу, которая считает общую цену. Вводится M рублей и N
+копеек, а также количество S товара Посчитайте общую цену в рублях и копейках
    за L товаров.
 """
 
 
 def total_sum(m, n, s):
+
     """Подсчет общей суммы покупок.
+
     :param m: рубли
+
     :param n: копейки
+
     :param s: количество товара
+
     :return: строка. общая цена в рублях и копейках. формат:
+
         'x rubles y kopecks'
+
     """
+
     # write your code here
     m, n, s = int(m), int(n), int(s)
     price = (m * 100 + n) * s  # in kopecks
     rubles = price // 100
     kopecks = price % 100
-    return print("Total price:", rubles, "rubles", kopecks, "kopecks")   # write return value here
-    #return rubles,kopecks
+    return print("Total price:", rubles, "rubles", kopecks, "kopecks")
+    # return rubles,kopecks
 
 
 if __name__ == '__main__':
     # здесь можно сделать ввод из консоли и проверить работу функции
-    m, n, s = input("Enter roubles, kopecks, quantity of goods separated by space: ").split()
+    m, n, s = input("Enter roubles, kopecks, "
+                    "quantity of goods separated by space: ").split()
     print(total_sum(m, n, s))
