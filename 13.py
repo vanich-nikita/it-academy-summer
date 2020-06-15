e794c5621845b49d1bebd3e49bfb2090964130f0
vanich-nikita <vanich_nikita@mail.ru>
2020-06-15 15:05:50 +0300

    homework1 fixes <Missed tasks descriptions blank spaces>

--- 13.py
+++ 13.py
@@ -2,6 +2,7 @@
 
 import unittest
 
+
 def median(pool):
     copy = sorted(pool)
     size = len(copy)
@@ -10,9 +11,11 @@ def median(pool):
     else:
         return (copy[int(size / 2 - 1)] + copy[int(size / 2)]) / 2
 
+
 class TestMedian(unittest.TestCase):
     def testMedian(self):
         self.assertEqual(median([2, 9, 9, 7, 9, 2, 4, 5, 8]), 7)
+
 
 if __name__ == '__main__':
     unittest.main()
