fbff116fd5032e02d8d50caa4baed877866bdf56
vanich-nikita <vanich_nikita@mail.ru>
2020-06-21 20:24:48 +0300

    homework2 version 1.19

--- src/homework2/task1.py
+++ src/homework2/task1.py
@@ -17,11 +17,11 @@ def total_sum(m, n, s):
 
     # write your code here
     m, n, s = int(m), int(n), int(s)
-    price = (m * 100 + n) * s  # in kopecks
-    rubles = price // 100
-    kopecks = price % 100
-    string = str(rubles) + " rubles " + str(kopecks) + " kopecks"
-    return string
+    # price = (m * 100 + n) * s  # in kopecks
+    # rubles = price // 100
+    # kopecks = price % 100
+    # string = str(((m * 100 + n) * s) // 100) + " rubles " + str(((m * 100 + n) * s) % 100) + " kopecks"
+    return str(((m * 100 + n) * s) // 100) + " rubles " + str(((m * 100 + n) * s) % 100) + " kopecks"
     # return rubles, kopecks
 
 
